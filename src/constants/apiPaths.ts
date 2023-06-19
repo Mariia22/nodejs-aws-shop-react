0961f44157369f83b17ebf0f139bdac45ce1d02e
Maria22 <m.ushakova22@gmail.com>
2023-06-19 13:21:09 +0200

    feat: add path to upload file

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://7ocvu9kesd.execute-api.eu-west-1.amazonaws.com",
   bff: "https://q2o5u95xj4.execute-api.eu-west-1.amazonaws.com",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev"
 };
