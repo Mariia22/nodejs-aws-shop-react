eb6caddd211c75db2894811688264155943fc842
Mariia Ushakova <mariia_ushakova@epam.com>
2026-05-23 17:39:12 +0200

    add import

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
-  product: "https://e3t3j5tlab.execute-api.us-east-1.amazonaws.com/prod",
+  product: "https://da8vlorzk0.execute-api.us-east-1.amazonaws.com/prod",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://218zj2c662.execute-api.us-east-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
